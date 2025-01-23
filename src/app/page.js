75954f29cfcc96b32c01bec230cb87a621eeb423
Easin2023 <mdeasinarafat016456@gmail.com>
2025-01-23 10:24:10 +0600

    google font added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+import { Playwrite_DE_Grund } from "next/font/google";
+
+const Playwrite = Playwrite_DE_Grund({ subsets: ["Playwrite_DE_Grund"] });
+
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time");
+  const data = await res.json();
+  return data.currentTime;
+};
+
 export default function Home() {
+  const time = getTime();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold text-center">
+    <div className=" ">
+      <h1 className="my-10 text-4xl font-bold text-center">
         Welcome to Hero Next App
       </h1>
+      <h1 className="text-2xl font-semibold text-center">Time now</h1>
+      <p className={`text-center text-5xl  ${Playwrite.className}`}>{time}</p>
     </div>
   );
 }
